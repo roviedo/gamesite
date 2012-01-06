ba553f511dfe1664785bede2ff5e30b581cafca9
roviedo <antoniorah@gmail.com>
2012-01-06 14:21:20 -0500

    Change base.html in templates directory

--- game_site/views.py
+++ game_site/views.py
@@ -4,6 +4,8 @@ from django.utils import simplejson
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from game_site.models import Login
+#from django.template.loader import Context
 
 def index(request, result = {}):
+    result = { ' head_title' : 'Gamesite', 'page_title' : 'Welcome to Game Site', 'page_body' : 'This is Gamesite' }
     return render_to_response('index.html', result)
